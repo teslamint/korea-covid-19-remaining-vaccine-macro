d993a8ee0767d515578c73c8caf7d5d5fd87cce6
SJang1 <git@sjang.dev>
2021-08-19 17:59:21 +0900

    test

--- kakao/cookie.py
+++ kakao/cookie.py
@@ -101,13 +101,7 @@ def load_cookie_from_chrome():
 
     cookieInput = input("모바일에서 확인한 쿠키값을 넣어주세요. : \n")
     jar = {'_kavacto': cookieInput}
-
-    cookie_dict = {}
-
-    # 쿠키를 cookie.ini 에 저장한다
-    for cookie in jar:
-        cookie_dict['_kavacto'] = cookie.value
-        dump_cookie(cookie.value)
-        break
+    cookie_dict['_kavacto'] = cookieInput
+    
 
     return cookie_dict
